3d7a46e3b4c37834d8dafcdd244c9dc1e8d18df2
chentianqihub <123949588+chentianqihub@users.noreply.github.com>
2024-09-16 11:36:41 +0800

    Update dnslookup.sh

--- scripts/dnslookup.sh
+++ scripts/dnslookup.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-# 定义变量
-DOWNLOAD_URL="https://github.com/ameshkov/dnslookup/releases/download/v1.10.1/dnslookup-linux-amd64-v1.10.1.tar.gz"
-TAR_FILE="dnslookup-linux-amd64-v1.10.1.tar.gz"
+DNSLOOKUP_VERSION=$(curl -s "https://api.github.com/repos/ameshkov/dnslookup/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+DOWNLOAD_URL="https://github.com/ameshkov/dnslookup/releases/download/${DNSLOOKUP_VERSION}/dnslookup-linux-amd64-${DNSLOOKUP_VERSION}.tar.gz"
+TAR_FILE="dnslookup-linux-amd64-${DNSLOOKUP_VERSION}.tar.gz"
 EXTRACT_DIR="linux-amd64"
 BIN_DIR="/usr/local/bin"
 BIN_FILE="$BIN_DIR/dnslookup"
+GRE='\033[0;32m'
+NC='\033[0m'   # (重置颜色)
 
 # 下载文件
 echo "Downloading $TAR_FILE..."
@@ -41,14 +43,11 @@ rm -rf "$EXTRACT_DIR"
 
 # 检查安装是否成功并输出第一行内容
 echo "Verifying installation..."
-FIRST_LINE=$(dnslookup --help 2>&1 | head -n 1)
+FIRST_LINE=$(dnslookup --help | head -n 1)
 if [ -z "$FIRST_LINE" ]; then
     echo "Error: dnslookup installation failed"
     exit 1
 else
-    # 使用 ANSI 转义序列将第一行内容设为红色
-    RED='\033[0;31m'
-    NC='\033[0m' # 没有颜色 (重置颜色)
-    echo -e "${RED}${FIRST_LINE}${NC}"
+    echo -e "${GRE}${FIRST_LINE}${NC}"
     echo "dnslookup installed successfully"
 fi
